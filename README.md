d73a0417a9c4731f91dd4daa6781e3abf4c43aa6
techsavvy42 <techsavvyblogs@gmail.com>
2024-02-18 13:21:27 -0700

    Removed useless files.

--- README.md
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e55c72c46a921bab3b68d82c707054cc9afef8eb2dec26df1cb8ba98cbd176c8
-size 2838
--- main.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:00e73cbb37a5fc7cb3efd9e87efb84a8e52fc2448f0d6487de50c0625abf31ed
-size 21435
